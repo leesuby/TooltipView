c9277690dabc12fd46ca4fa96dc78133ff43d9fa
Long <nguyencaothienlong2001@gmail.com>
2022-12-20 16:44:10 +0700

    init popView

--- TooltipView/TooltipView/AppDelegate.swift
+++ TooltipView/TooltipView/AppDelegate.swift
@@ -10,21 +10,32 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 13, *) {
+            return true
+        } else {
+            let vc = ViewController()
+            
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.window?.rootViewController = vc
+            appDelegate.window?.makeKeyAndVisible()
+        }
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- TooltipView/TooltipView/ViewController.swift
+++ TooltipView/TooltipView/ViewController.swift
@@ -8,10 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    private var toolTip: PopTip = {
+        let toolTip = PopTip()
+        toolTip.font = UIFont(name: "Avenir-Medium", size: 12)!
+        toolTip.shouldDismissOnTap = true
+        toolTip.shouldDismissOnTapOutside = true
+        toolTip.shouldDismissOnSwipeOutside = true
+        toolTip.shouldConsiderCutoutTapSeparately = true
+        toolTip.edgeMargin = 5
+        toolTip.offset = 2
+        toolTip.bubbleOffset = 0
+        toolTip.edgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        return toolTip
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        initView()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+    }
+    
+    @objc func didTapButton(){
+        toolTip.show(text: "muahahahahahaha", direction: .auto, maxWidth: 100, in: view, from: button.frame)
+    }
+
+    func initView(){
+        view.backgroundColor = .white
+        
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
     }
 
 
